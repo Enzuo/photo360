a1ba7127c4c02e9467560f3c313e95696b38e6ac
Enzuo <potc.zone+git@gmail.com>
2020-02-02 18:50:13 +0100

    Read files in images folder

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,20 @@
 var remote = require('electron').remote
 var PhotoSphereViewer = require('photo-sphere-viewer')
+var fs = require('fs')
+const path = require('path')
+
 
 var viewer = new PhotoSphereViewer({
   container: 'viewer',
-  panorama: '../images/strad_salon3v2.jpg'
+  // panorama: '../images/strad_salon3v2.jpg'
 })
+
+
+var dirPath = './images'
+var images = fs.readdirSync(dirPath)
+var index = 0;
+console.log(images)
+viewer.setPanorama(path.join(__dirname, '../', dirPath, images[index]))
+
+
+console.log('hello')
