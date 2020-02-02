1214dc031066a3a0ff5652c0d2cb9f766a09a75a
Enzuo <potc.zone+git@gmail.com>
2020-02-02 22:46:00 +0100

    Use native Image to create smaller thumbnails

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,8 @@
-var remote = require('electron').remote
+var { remote, nativeImage } = require('electron')
 var PhotoSphereViewer = require('photo-sphere-viewer')
 var fs = require('fs')
 const path = require('path')
+
 
 
 var viewer = new PhotoSphereViewer({
@@ -25,10 +26,11 @@ function changePanorama(index){
   viewer.setPanorama(path.join(__dirname, '../', dirPath, images[index]))
 }
 
-function setUpBrowser(images) {
+async function setUpBrowser(images) {
   var browser = document.getElementById('browser-elem')
   browser.removeEventListener('click', browserElemClick)
   browser.innerHTML = ''
+  console.log('start')
   
   for(var i=0; i<images.length; i++){
     var element = document.createElement('li')
@@ -36,13 +38,33 @@ function setUpBrowser(images) {
     element.setAttribute('index', i)
     var bg = document.createElement('div')
     bg.className = 'bg'
-    bg.setAttribute('style', "background-image: url('../images/"+images[i]+"')")
+    var thumb = createThumb('./images/'+images[i])
+    bg.setAttribute('style', "background-image: url('data:image/png;base64,"+thumb+"')")
     bg.setAttribute('index', i)
     element.appendChild(bg)
     browser.appendChild(element)
   }
 
+  console.log('end')
+
   browser.addEventListener('click', browserElemClick)
+}
+
+function createThumb(filepath){
+  var natImg = nativeImage.createFromPath(filepath)
+  
+  var size = natImg.getSize()
+  var smallestSide = Math.min(size.height, size.width)
+  var cropSquareSize = parseInt(smallestSide / 2)
+  var x = size.width / 2 - cropSquareSize / 2
+  var y = size.height / 2 - cropSquareSize / 2
+  console.log(size, x, y, cropSquareSize)
+  natImg = natImg.crop({x:parseInt(x), y:parseInt(y), width:cropSquareSize, height:cropSquareSize})
+
+  natImg = natImg.resize({width: 100, height: 100, quality:'good'})
+  var bufferImg = natImg.toJPEG(70)
+  var img64 = bufferImg.toString('base64');
+  return img64
 }
 
 function browserElemClick(e) {
